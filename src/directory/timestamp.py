0134e301f60289f632e098daf84eb275835c674b
Jerome Pinguet <jerome@jerome.cc>
2023-12-21 05:58:24 +0300

    fix update neo4j records timestamp

--- src/directory/timestamp.py
+++ src/directory/timestamp.py
@@ -1,15 +1,17 @@
 import logging
+import uuid
 
 from neomodel import db
 
 logger = logging.getLogger(__name__)
 
-def update_contact_timestamp(uid):
-    logger.warn(f"update_contact_timestamp({uid})")
-    query=f"""MATCH (f:Facility) WHERE f.uid="{uid}" SET f.contactUpdatedAt=timestamp() RETURN f;"""
+def update_contact_timestamp(uid: uuid.UUID):
+    # matching a node requires hex string representation of uuid4 with no dash
+    query=f"""MATCH (f:Facility) WHERE f.uid="{uid.hex}" SET f.contactUpdatedAt=timestamp() RETURN f;"""
     results, cols = db.cypher_query(query)
-    logger.warn(results)
     if results:
         return
+    # matching LOCATION relationship requires the dashed string representation
+    # of uuid4
     query=f"""MATCH (f:Facility)-[rel:LOCATION]-(e:Effector) WHERE rel.uid="{uid}" SET rel.contactUpdatedAt=timestamp();"""
     results, cols = db.cypher_query(query)
