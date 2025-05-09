0165e9c6650364bc55a3ccaf9d72be8b4e580ea6
Jerome Pinguet <jerome@jerome.cc>
2025-05-09 19:41:58 +0300

    WIP typing

--- src/api/types/geography.py
+++ src/api/types/geography.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel, Field
 
 class AdministrativeTerritorialEntityOfFrance(BaseModel):
     uid: str
-    name_en: str = Field(exclude=True)
+    name_en: str|None = Field(exclude=True)
     name_fr: str
-    slug_en: str = Field(exclude=True)
+    slug_en: str|None = Field(exclude=True)
     slug_fr: str
     wikidata: str
 
