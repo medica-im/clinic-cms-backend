37e74e2c51e9d1a0ec62bd6c14a57b96e82aceb7
Jerome Pinguet <jerome@jerome.cc>
2025-05-09 19:13:29 +0300

    WIP typing

--- src/api/types/organization.py
+++ src/api/types/organization.py
@@ -10,8 +10,6 @@ class OrganizationPy(BaseModel):
     name_en: str|None = Field(exclude=True)
     label_fr: str
     label_en: str|None = Field(exclude=True)
-    synonyms_fr: list[str]|None
-    synonyms_en: list[str]|None = Field(exclude=True)
     #type: OrganizationTypePy
     #organization: OrganizationPy | None
     #commune: str
