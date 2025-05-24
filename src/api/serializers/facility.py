a65ad007922a996fd053ad9035f6f54f8c3c4560
Jerome Pinguet <jerome@jerome.cc>
2025-05-24 14:35:27 +0300

    WIP

--- src/api/serializers/facility.py
+++ src/api/serializers/facility.py
@@ -39,6 +39,7 @@ def get_facilities(
         active: bool = True,
     ) -> list[FacilityPy]:
     if uid:
+            logger.debug(uid)
             query=(
                 f"""
                 MATCH (f:Facility)-[:LOCATED_IN_THE_ADMINISTRATIVE_TERRITORIAL_ENTITY]->(c:Commune)-[:LOCATED_IN_THE_ADMINISTRATIVE_TERRITORIAL_ENTITY]->(dpt:DepartmentOfFrance), (f)-[]-(entry:Entry), (e:Effector)-[]-(entry)-[]-(et:EffectorType)
