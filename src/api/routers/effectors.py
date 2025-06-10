a217e274bfcaf4b86be93dee94ba828592678ab6
Jerome Pinguet <jerome@jerome.cc>
2025-06-10 04:09:32 +0300

    wip

--- src/api/routers/effectors.py
+++ src/api/routers/effectors.py
@@ -6,8 +6,8 @@ from pydantic import ValidationError
 router = APIRouter()
 
 @router.get("/effectors")
-async def organization_types() -> list[Effector]:
-    return get_effectors()
+async def organization_types(effector_type: str|None = None, facility: str|None = None ) -> list[Effector]:
+    return get_effectors(effector_type=effector_type, facility=facility)
 
 @router.get("/effectors/{uid}")
 async def organization_type(uid: str)->Effector:
