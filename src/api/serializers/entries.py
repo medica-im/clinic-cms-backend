feeea3a25da1067339f48052aaab4aa75ed99332
Jerome Pinguet <jerome@jerome.cc>
2025-06-21 02:47:40 +0300

    wip

--- src/api/serializers/entries.py
+++ src/api/serializers/entries.py
@@ -27,7 +27,7 @@ def get_entries(
     )->list[str]:
     if effector_type and facility and effector:
         query=(
-            f"""MATCH (entry:Entry)-[:HAS_EFFECTOR]->(effector:Effector) WHERE effector.uid="{effector}" MATCH (entry)-[:HAS_FACILITY]->(f:Facility) WHERE f.uid="{facility}", (entry)-[:HAS_EFFECTOR_TYPE]->(et:EffectorType) WHERE et.uid="{effector_type}" RETURN DISTINCT entry.uid;"""
+            f"""MATCH (entry:Entry)-[:HAS_EFFECTOR]->(effector:Effector) WHERE effector.uid="{effector}" MATCH (entry)-[:HAS_FACILITY]->(f:Facility) WHERE f.uid="{facility}" MATCH (entry)-[:HAS_EFFECTOR_TYPE]->(et:EffectorType) WHERE et.uid="{effector_type}" RETURN DISTINCT entry.uid;"""
         )
     elif effector_type and facility:
         query=(
