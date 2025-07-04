ded9938c9ea932f119e1029c9cee9298bf9e9689
Jerome Pinguet <jerome@jerome.cc>
2025-07-04 16:18:59 +0300

    wip

--- src/heatwave/urls.py
+++ src/heatwave/urls.py
@@ -2,15 +2,13 @@ from django.urls import include, path
 from rest_framework import routers
 from heatwave import views
 
-router = routers.DefaultRouter()
-
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 app_name = 'heatwave'
 
 urlpatterns = [
     path(
-        'vigilance/<str:pk>',
+        'warning/<str:pk>',
         views.heatwave_department.as_view(),
     )
 ]
