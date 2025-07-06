0cc7897a1bb786d710110e5a69aae4c1cc57267a
Jerome Pinguet <jerome@jerome.cc>
2025-07-06 12:18:50 +0300

    fix duplicate entries bug

--- src/directory/utils.py
+++ src/directory/utils.py
@@ -474,7 +474,7 @@ def get_entries(
         WITH *
         MATCH (e:Effector)-[rel:LOCATION]-(f:Facility)
         OPTIONAL MATCH (entry:Entry)-[:MEMBER_OF]->(o:Organization)
-        RETURN entry,e,et,f,commune,rel,o;
+        RETURN COLLECT(DISTINCT entry) AS entry,e,et,f,commune,rel,o;
         """
     results, cols = db.cypher_query(query)
     if results:
