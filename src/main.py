1b3825798446547174808e1cab74a90111e90f0b
Jerome Pinguet <jerome@jerome.cc>
2025-07-15 17:19:52 +0300

    WIP

--- src/main.py
+++ src/main.py
@@ -31,10 +31,10 @@ oidc = OpenIdConnect(openIdConnectUrl=settings.OPEN_ID_CONNECT_URL)
 
 app = FastAPI(
     swagger_ui_init_oauth = {
-        "clientId": settings.OPENAPI_CLIENT_ID, 
-        "appName": "test", 
+        "clientId": "docs", 
+        "appName": "Doc Tools", 
         "usePkceWithAuthorizationCodeGrant": True, 
-        "scopes": "openid",
+        "scopes": "openid profile",
     }
 )
 
