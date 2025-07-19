a63347f6e0f1547aa416fbd5c0c2d7650a43fd90
Jerome Pinguet <jerome@jerome.cc>
2025-07-19 19:02:12 +0300

    WIP

--- src/api/routers/auth.py
+++ src/api/routers/auth.py
@@ -102,16 +102,16 @@ async def auth_google(request: Request, credential: str|None = None):
         value=refresh_token,
         path="/",
         httponly=False,
-        samesite='lax',
-        domain='localhost'
+        secure=True,
+        samesite='none'
     )
     response.set_cookie(
         key="access-token",
         value=access_token,
         path="/",
         httponly=False,
-        samesite='lax',
-        domain='localhost'
+        secure=True,
+        samesite='none'
     )
     return response
 
