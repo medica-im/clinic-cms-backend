b50deda6a3ec8b40ac5a52e240615147398707c3
Jerome Pinguet <jerome@jerome.cc>
2025-07-19 18:36:13 +0300

    WIP

--- src/main.py
+++ src/main.py
@@ -51,8 +51,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Cookie"],
 )
 
 app.include_router(organizations.router)
