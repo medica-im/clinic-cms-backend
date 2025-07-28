8d4097a061885dfe354599dc0163cd8bb03ad674
Jerome Pinguet <jerome@jerome.cc>
2025-07-28 07:58:09 +0200

    WIP

--- src/api/routers/phones.py
+++ src/api/routers/phones.py
@@ -11,9 +11,11 @@ router = APIRouter()
 
 @router.put("/phones/{item_id}", response_model=Phone)
 async def update_item(item_id: str, item: Phone):
+    logger.debug(item)
     update_item_encoded = jsonable_encoder(item)
+    logger.debug(update_item_encoded)
     try:
-        phone_number = PhoneNumber.objects.get(id=update_item_encoded.id)
+        phone_number = PhoneNumber.objects.get(id=item_id)
         logger.debug(phone_number)
     except PhoneNumber.DoesNotExist:
         raise HTTPException(status_code=404, detail=f"PhoneNumber not found")
