0f8bcdc6b84a243d3e40974591708174b7eaa120
Jerome Pinguet <jerome@jerome.cc>
2025-07-29 02:15:06 +0200

    wip

--- src/api/routers/effectors.py
+++ src/api/routers/effectors.py
@@ -29,5 +29,6 @@ async def effector(uid: str)->Effector:
 
 @router.post("/effectors/", status_code=status.HTTP_201_CREATED)
 async def post_effector(effector: EffectorPost, request: Request, jwt: Annotated[dict, Depends(JWT)]) -> Effector:
+    logger.debug(f"hello post_effector {effector}")
     await authorize_api("effectors_v2", request, jwt)
     return await create_effector(effector.model_dump())
