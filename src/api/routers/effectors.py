20aa0acf42002f39134b3499773e9c5a32e776a2
Jerome Pinguet <jerome@jerome.cc>
2025-07-30 03:43:06 +0200

    WIP

--- src/api/routers/effectors.py
+++ src/api/routers/effectors.py
@@ -7,9 +7,8 @@ from pydantic import ValidationError
 #from api.auth import JWT
 from api.auth import authorize_api
 from fastapi_nextauth_jwt import NextAuthJWT
-from api.logger import get_logger
 
-logger = get_logger(__name__)
+logger = logging.getLogger(__name__)
 
 auth_secret=os.getenv("AUTH_SECRET")
 if auth_secret:
