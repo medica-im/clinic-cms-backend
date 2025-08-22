b50899e71c3b6988747726c337c5234314bb5bf5
Jerome Pinguet <jerome@jerome.cc>
2025-08-22 09:58:55 +0200

    wip

--- src/directory/tasty/facilities.py
+++ src/directory/tasty/facilities.py
@@ -179,6 +179,7 @@ class FacilityResource(Resource):
         return self.get_object_list(bundle.request)
 
     def obj_get(self, bundle, **kwargs):
+        logger.debug(f"request: {bundle.request.META.__dict__}")
         directory=get_directory(bundle.request)
         slug= kwargs['slug']
         facilities = get_facilities(directory=directory, slug=slug)
