e5fb278939a6656bde75b5d3f4a7a5fd37253d61
Jerome Pinguet <jerome@jerome.cc>
2025-08-28 11:07:12 +0200

    wip

--- src/api/serializers/entries.py
+++ src/api/serializers/entries.py
@@ -117,7 +117,7 @@ async def get_entry(uid:str)->Entry:
 async def update_entry(uid:str, update_data: dict[str, Any]):
     logger.debug(update_data)
     entry = await EntryAgraph.nodes.get(uid=uid)
-    if update_data['carte_vitale']:
+    if update_data['carte_vitale'] != None:
         entry.carte_vitale=update_data['carte_vitale']
         await entry.save()
     return Entry.model_validate(entry.__properties__)
