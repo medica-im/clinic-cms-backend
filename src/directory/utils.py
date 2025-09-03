8ac1ac3d6ba0ee402c9ced117b45deb6202e7aa1
Jerome Pinguet <jerome@jerome.cc>
2025-09-03 13:25:44 +0200

    wip

--- src/directory/utils.py
+++ src/directory/utils.py
@@ -117,6 +117,7 @@ def get_appointments_neomodel(entry: str, nodes: list[Appointment]):
             return 'office'
         else:
             return None
+    logger.debug(nodes)
     if not nodes:
         return None
     data = [
