67ba64e5d4636c924505eef645010a9ad7ea2345
Jerome Pinguet <jerome@jerome.cc>
2025-09-10 17:37:42 +0200

    wip

--- src/api/serializers/entries.py
+++ src/api/serializers/entries.py
@@ -107,6 +107,12 @@ def create_entry(dir_name, kwargs)-> str:
         Contact.objects.create(neomodel_uid=entry.uid)
     except IntegrityError:
         pass
+    if "HCW" in effector_type.labels():
+        query = f"""MATCH (e:Effector) WHERE e.uid="{effector.uid}" SET e:HealthWorker RETURN labels(e);"""
+        result = db.cypher_query(query)
+        logger.debug(result[0][0])
+        if "HealthWorker" not in result[0][0]:
+            raise HTTPException(status_code=500, detail=f"Label 'HealthWorker' not applied to Effector {effector.uid} of type {effector_type.name_fr}")
     return str(entry.uid)
 
 async def get_entry(uid:str)->Entry:
