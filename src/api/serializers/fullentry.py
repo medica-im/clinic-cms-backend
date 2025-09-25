3fd5c2479929c11e9bf1d6959574f50b7334ba68
Jerome Pinguet <jerome@jerome.cc>
2025-09-25 07:44:18 +0200

    wip

--- src/api/serializers/fullentry.py
+++ src/api/serializers/fullentry.py
@@ -3,7 +3,7 @@ from directory.tasty.fulleffectors import createEffectorRessource
 from api.types.fullentry import FullEntry
 
 async def get_fullentry(uid):
-    entry_node = find_entry(uid)
+    entry_node = find_entry(uid=uid)
     entry_object = createEffectorRessource(entry_node)
     entry_pydantic = FullEntry.model_validate(entry_object)
     return entry_pydantic
