2f93fa6e372190f8a6afbe7034a5c00d6a20190f
Jerome Pinguet <jerome@jerome.cc>
2025-10-18 16:27:24 +0200

    slugfullentries API endpoint

--- src/api/routers/fullentries.py
+++ src/api/routers/fullentries.py
@@ -2,6 +2,7 @@ import logging
 from typing import Annotated
 from fastapi import APIRouter, status, Request, Depends
 from api.serializers.fullentry import get_fullentry
+from api.serializers.slug_fullentry import slug_find_entry
 from api.types.fullentry import FullEntry
 from api.utils import get_directory_from_hostname
 from api.auth import authorize_api
@@ -14,3 +15,7 @@ router = APIRouter()
 @router.get("/fullentries/{uid}")
 def get(uid: str) -> FullEntry:
     return get_fullentry(uid)
+
+@router.get("/slugfullentries/{type}/{commune}/{effector}")
+async def get_slug_fullentries(type: str, commune: str, effector: str) -> FullEntry|None:
+    await slug_find_entry(commune, effector, type)
