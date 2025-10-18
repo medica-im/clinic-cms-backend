831f6131dc7b04e11433cd9bd95cec2533661bf9
Jerome Pinguet <jerome@jerome.cc>
2025-10-18 16:31:15 +0200

    fix typo

--- src/api/serializers/slug_fullentry.py
+++ src/api/serializers/slug_fullentry.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 async def slug_find_entry(commune: str, effector: str, type: str)->str:
     query=(
-        f"""MATCH (e:Entry)-[:HAS_FACILITY]->[f:Facility]-[:LOCATED_IN_THE_ADMINISTRATIVE_TERRITORIAL_ENTITY]->[c:Commune], (e)-[:HAS_EFFECTOR]->(effector:Effector), (e)-[:HAS_EFFECTOR_TYPE]->(et:EffectorType)
+        f"""MATCH (e:Entry)-[:HAS_FACILITY]->(f:Facility)-[:LOCATED_IN_THE_ADMINISTRATIVE_TERRITORIAL_ENTITY]->(c:Commune), (e)-[:HAS_EFFECTOR]->(effector:Effector), (e)-[:HAS_EFFECTOR_TYPE]->(et:EffectorType)
         WHERE c.slug_fr="{commune}" AND effector.slug_fr="{effector}" AND et.slug_fr="{type}" RETURN e.uid;"""
     )
     results, cols = await adb.cypher_query(query)
