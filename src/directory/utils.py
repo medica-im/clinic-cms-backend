10e66876d91cc9b3eb295250c909bd7210b995ab
Jerome Pinguet <jerome@jerome.cc>
2025-10-18 19:54:38 +0200

    slugfullentries WIP async

--- src/directory/utils.py
+++ src/directory/utils.py
@@ -229,20 +229,22 @@ def get_websites_neomodel(
         Serializer=WebsiteSerializer
     )
 
-async def async_get_websites_neomodel(
-        entry: Entry|None=None,
-        e: Effector | None = None,
-        ef: EffectorFacility | None = None,
-        f: Facility | None = None
-    ):
-    return await async_get_contact_related_neomodel(
-        entry=entry,
-        e=e,
-        ef=ef,
-        f=f,
-        attribute="websites",
-        Serializer=AsyncWebsiteSerializer
-    )
+async def async_get_websites_neomodel(entry: Entry):
+    contact = await Contact.objects.prefetch_related('websites', 'websites__roles').aget(neomodel_uid=entry.uid)
+    websites = []
+    async for website in contact.websites.all():
+        roles= []
+        async for role in website.roles.all():
+            roles.append(role)
+        websites.append(
+            {
+                "id": website.id,
+                "url": websites.url,
+                "roles": roles
+            }
+        )
+    logger.debug(f"{websites=}")
+    return websites or None
 
 def get_socialnetworks_neomodel(
         entry: Entry|None=None,
