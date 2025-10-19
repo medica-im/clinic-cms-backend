f78d4f63a00d1c57c3d99389c42a5bababec371e
Jerome Pinguet <jerome@jerome.cc>
2025-10-19 05:12:39 +0200

    fix address in facilities_uid

--- src/directory/utils.py
+++ src/directory/utils.py
@@ -598,7 +598,7 @@ def get_facilities(
     if uid:
         query=f"""MATCH (f:Facility)-[:LOCATED_IN_THE_ADMINISTRATIVE_TERRITORIAL_ENTITY]->(commune:Commune)-[:LOCATED_IN_THE_ADMINISTRATIVE_TERRITORIAL_ENTITY*]->(country:Country) WHERE f.uid="{uid}" RETURN f,commune,country;"""
     elif slug:
-        query=f"""MATCH (d:Directory)-[:HAS_ENTRY]->(e:Entry),(e)-[:HAS_EFFECTOR]->(:Effector),(e)-[:HAS_FACILITY]->(f:Facility)-[]->(commune:Commune)-[:LOCATED_IN_THE_ADMINISTRATIVE_TERRITORIAL_ENTITY*]->(country:Country) WHERE f.slug="{slug}" AND d.name="{directory.name}" AND e.active={str(active).lower()} RETURN f,commune,country;"""
+        query=f"""MATCH (d:Directory)-[:HAS_ENTRY]->(e:Entry),(e)-[:HAS_EFFECTOR]->(:Effector),(e)-[:HAS_FACILITY]->(f:Facility)-[:LOCATED_IN_THE_ADMINISTRATIVE_TERRITORIAL_ENTITY]->(commune:Commune)-[:LOCATED_IN_THE_ADMINISTRATIVE_TERRITORIAL_ENTITY*]->(country:Country) WHERE f.slug="{slug}" AND d.name="{directory.name}" AND e.active={str(active).lower()} RETURN f,commune,country;"""
     else:
         query=f"""MATCH (d:Directory)-[:HAS_ENTRY]->(e:Entry),
         (e)-[:HAS_EFFECTOR]->(:Effector),
