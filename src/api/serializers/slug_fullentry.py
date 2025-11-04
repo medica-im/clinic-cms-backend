eded751c07cc80e257d714cc9180762fb14bc234
Jerome Pinguet <jerome@jerome.cc>
2025-11-04 06:26:00 +0100

    return only active entry

--- src/api/serializers/slug_fullentry.py
+++ src/api/serializers/slug_fullentry.py
@@ -3,10 +3,10 @@ from neomodel import adb
 from fastapi import HTTPException
 logger = logging.getLogger(__name__)
 
-async def slug_find_entry(commune: str, effector: str, type: str)->str:
+async def slug_find_entry(commune: str, effector: str, type: str, active: bool = True)->str:
     query=(
         f"""MATCH (e:Entry)-[:HAS_FACILITY]->(f:Facility)-[:LOCATED_IN_THE_ADMINISTRATIVE_TERRITORIAL_ENTITY]->(c:Commune), (e)-[:HAS_EFFECTOR]->(effector:Effector), (e)-[:HAS_EFFECTOR_TYPE]->(et:EffectorType)
-        WHERE c.slug_fr="{commune}" AND effector.slug_fr="{effector}" AND et.slug_fr="{type}" RETURN e.uid;"""
+        WHERE c.slug_fr="{commune}" AND effector.slug_fr="{effector}" AND et.slug_fr="{type} AND e.active={str(active)} RETURN e.uid;"""
     )
     results, cols = await adb.cypher_query(query)
     logger.debug(f"{results=}\n{cols=}")
