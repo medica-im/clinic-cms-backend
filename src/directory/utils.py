6319d32386e74863282e49dd8a9030488400a881
Jerome Pinguet <jerome@jerome.cc>
2025-11-25 16:59:22 +0100

    fix

--- src/directory/utils.py
+++ src/directory/utils.py
@@ -907,7 +907,7 @@ def get_uid_query(uid: str):
         WITH *, bLabels + labels(et) AS allLabels
         UNWIND allLabels AS labelList
         UNWIND labelList AS label
-        RETURN entry,et,e,rel,f,c,country,tpp,COLLECT(DISTINCT pm) AS pm,convention,COLLECT(DISTINCT a) AS a,DISTINCT label AS typelabels,COLLECT(DISTINCT memberships) as memberships;"""
+        RETURN entry,et,e,rel,f,c,country,tpp,COLLECT(DISTINCT pm) AS pm,convention,COLLECT(DISTINCT a) AS a,label,COLLECT(DISTINCT memberships) as memberships;"""
 
 def get_slug_query(directory, effector_slug, effector_type_slug, facility_slug):
     return f"""MATCH (d:Directory) WHERE d.name="{directory.name}"
@@ -934,7 +934,7 @@ def get_slug_query(directory, effector_slug, effector_type_slug, facility_slug):
             WITH *, bLabels + labels(et) AS allLabels
             UNWIND allLabels AS labelList
             UNWIND labelList AS label
-            RETURN entry,et,e,rel,f,c,country,tpp,COLLECT(DISTINCT pm) AS pm,convention,COLLECT(DISTINCT a) AS a, DISTINCT label as typelabels,COLLECT(DISTINCT memberships) as memberships;
+            RETURN entry,et,e,rel,f,c,country,tpp,COLLECT(DISTINCT pm) AS pm,convention,COLLECT(DISTINCT a) AS a, label,COLLECT(DISTINCT memberships) as memberships;
             """
 
 def entry_dict(results, cols):
