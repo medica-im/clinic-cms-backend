68b884138ec9dddb7e04331edd63758bd47ca697
Jerome Pinguet <jerome@jerome.cc>
2025-11-25 17:16:37 +0100

    fix

--- src/directory/utils.py
+++ src/directory/utils.py
@@ -906,7 +906,7 @@ def get_uid_query(uid: str):
         WITH *, et, collect(DISTINCT labels(b)) AS bLabels
         WITH *, bLabels + labels(et) AS allLabels
         UNWIND allLabels AS labelList
-        RETURN entry,et,e,rel,f,c,country,tpp,pm,convention,COLLECT(DISTINCT a) AS a,labelList,COLLECT(DISTINCT memberships) as memberships;"""
+        RETURN entry,et,e,rel,f,c,country,tpp,pm,convention,a,labelList,memberships;"""
 
 def get_slug_query(directory, effector_slug, effector_type_slug, facility_slug):
     return f"""MATCH (d:Directory) WHERE d.name="{directory.name}"
@@ -933,7 +933,7 @@ def get_slug_query(directory, effector_slug, effector_type_slug, facility_slug):
             WITH *, bLabels + labels(et) AS allLabels
             UNWIND allLabels AS labelList
             UNWIND labelList AS label
-            RETURN entry,et,e,rel,f,c,country,tpp,COLLECT(DISTINCT pm) AS pm,convention,COLLECT(DISTINCT a) AS a, label,COLLECT(DISTINCT memberships) as memberships;
+            RETURN entry,et,e,rel,f,c,country,tpp,COLLECT(DISTINCT pm) AS pm,convention,a, label,COLLECT(DISTINCT memberships) as memberships;
             """
 
 def entry_dict(results, cols):
