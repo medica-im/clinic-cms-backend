f984944a81c40f4f1eb4e610be4c110429dc174e
Jerome Pinguet <jerome@jerome.cc>
2025-11-27 15:49:24 +0100

    queryfullentries api endpoint

--- src/api/routers/fullentries.py
+++ src/api/routers/fullentries.py
@@ -2,7 +2,7 @@ import logging
 from typing import Annotated
 from fastapi import APIRouter, status, Request, Depends
 from api.serializers.fullentry import get_fullentry, async_get_fullentry
-from api.serializers.slug_fullentry import slug_find_entry
+from api.serializers.slug_fullentry import slug_find_entry, query_find_entry
 from api.types.fullentry import FullEntry
 from api.auth import authorize_api
 from api.auth import JWT
@@ -18,4 +18,9 @@ def get(uid: str) -> FullEntry:
 @router.get("/slugfullentries/{type}/{commune}/{effector}")
 async def get_slug_fullentries(type: str, commune: str, effector: str) -> FullEntry|None:
     uid = await slug_find_entry(commune, effector, type)
+    return await async_get_fullentry(uid)
+
+@router.get("/queryfullentries/")
+async def get_query_fullentries(effector: str, facility: str, type: str) -> FullEntry|None:
+    uid = await query_find_entry(effector, facility, type)
     return await async_get_fullentry(uid)
--- src/api/serializers/slug_fullentry.py
+++ src/api/serializers/slug_fullentry.py
@@ -15,3 +15,16 @@ async def slug_find_entry(commune: str, effector: str, type: str, active: bool =
     uid = results[0][0]
     logger.debug(f"{uid=}")
     return uid
+
+async def query_find_entry(effector: str, facility: str, type: str, active: bool = True)->str:
+    query=(
+        f"""MATCH (e:Entry)-[:HAS_FACILITY]->(f:Facility), (e)-[:HAS_EFFECTOR]->(effector:Effector), (e)-[:HAS_EFFECTOR_TYPE]->(et:EffectorType)
+        WHERE f.slug="{facility}" AND effector.slug_fr="{effector}" AND et.slug_fr="{type}" AND e.active={str(active)} RETURN e.uid;"""
+    )
+    results, cols = await adb.cypher_query(query)
+    logger.debug(f"{results=}\n{cols=}")
+    if not results:
+        raise HTTPException(status_code=404, detail="Entry not found")
+    uid = results[0][0]
+    logger.debug(f"{uid=}")
+    return uid
