b6010d78ef49e234c077daf11a10a62557fd6581
Jerome Pinguet <jerome@jerome.cc>
2025-12-10 18:42:59 +0100

    fix install labels

--- src/directory/management/commands/install_labels.py
+++ src/directory/management/commands/install_labels.py
@@ -2,17 +2,14 @@ from django import setup as setup_django
 from django.core.management.base import BaseCommand
 import importlib.util
 import sys
-from neomodel import install_all_labels, install_labels
+import os
+from neomodel.scripts import neomodel_install_labels
+
 
 class Command(BaseCommand):
     help = 'Install labels and constraints for your neo4j database'
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            '--class',
-            type=str,
-            help="Class name"
-        )
         parser.add_argument(
             '--module',
             type=str,
@@ -20,9 +17,8 @@ class Command(BaseCommand):
         )
     def handle(self, *args, **options):
         setup_django()
-        class_name = options["class"]
         module_name = options["module"]
-        if class_name and module_name:
+        if module_name:
             if module_name in sys.modules:
                 print(f"{module_name!r} already in sys.modules")
                 module = sys.modules[module_name]
@@ -34,16 +30,4 @@ class Command(BaseCommand):
                 print(f"{module_name!r} has been imported")
             else:
                 print(f"can't find the {module_name!r} module")
-    
-            the_class = getattr(module, class_name)
-    
-            if class_name and module_name and the_class:
-                install_labels(
-                    the_class,
-                    quiet=False,
-                    stdout=self.stdout
-                )
-        else:
-            install_all_labels(
-                stdout=self.stdout
-            )
+            os.popen("neomodel_install_labels module_name")
