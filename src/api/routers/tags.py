4127567b2ee40037363f841b468bc732f221670a
Jerome Pinguet <jerome@jerome.cc>
2025-12-23 10:31:36 +0100

    wip tag api

--- src/api/routers/tags.py
+++ src/api/routers/tags.py
@@ -23,5 +23,5 @@ async def get_tag_category(uid: str) -> TagCategory:
     return await tag_category(uid)
 
 @router.get("/tags", response_model=list[Tag])
-async def get_tags(category: str|None):
+async def get_tags(category: str|None=None):
     return await tags(category)
