783c43a906a95e7ab50da720a9d15825d940bffa
Jerome Pinguet <jerome@jerome.cc>
2026-01-06 01:15:59 +0100

    public_holidays_zone

--- src/api/utils.py
+++ src/api/utils.py
@@ -114,6 +114,7 @@ async def get_ttl(api_version: str, request):
     #path = request.scope['root_path'] + request.scope['route'].path
     path = request.scope['route'].path
     endpoint = "%s:%s" % (api_version, path)
+    logger.debug(f"{endpoint=}")
     site = await get_site_from_request(request)
     try:
         ttl_obj = await TTL.objects.filter(endpoint__name=endpoint,site=site).afirst()
