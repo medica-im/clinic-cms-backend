1e165fdc41e3cbb7209474213fa97f36f3ca119e
Jerome Pinguet <jerome@jerome.cc>
2026-01-06 13:53:43 +0100

    clear cache after phone post

--- src/api/routers/phones.py
+++ src/api/routers/phones.py
@@ -47,6 +47,7 @@ async def update_item(item_id: str, item: Phone, request: Request, jwt: Annotate
         await phone_number.roles.aset(roles)
     phone_number = await PhoneNumber.objects.prefetch_related("roles").aget(id=item_id)
     logger.debug(f'after aset: {phone_number.roles.all()}')
+    await clear_cache('v2:entries', request)
     return i
 
 @router.get("/phones/{item_id}", response_model=Phone)
