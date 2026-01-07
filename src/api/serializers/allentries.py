8d8bd613e166a0f74ca46e77658c7a5aeeb5fc52
Jerome Pinguet <jerome@jerome.cc>
2026-01-07 09:25:11 +0100

    scrub

--- src/api/serializers/allentries.py
+++ src/api/serializers/allentries.py
@@ -262,31 +262,33 @@ def process(entries):
         "administrator": entries
     }
     for r in ["staff", "anonymous"]:
-        logger.debug(r)
+        logger.debug(f"{'*'*(len(r)+4)}\n* {r} *\n {'*'*(len(r)+4)}")
         scrubbed_entries = []
         for entry in entries:
             phones = entry.phones            
             if phones:
-                logger.debug([(phone["phone"], [r["name"] for r in phone["roles"]]) for phone in phones])
-                phones_count=len(phones)
                 new_phones = [
                     phone
                     for phone in phones
                     if (r in [role["name"] for role in phone["roles"]])
                 ]
-                logger.debug([(phone["phone"], [r["name"] for r in phone["roles"]]) for phone in new_phones])
+                
                 new_phones_count=len(new_phones)
+                phones_count=len(phones)
                 if new_phones_count != phones_count:
                     logger.debug(f"{phones_count-new_phones_count} phone(s) removed!")
+                    logger.debug([(phone["phone"], [r["name"] for r in phone["roles"]]) for phone in new_phones])
+                    logger.debug([(phone["phone"], [r["name"] for r in phone["roles"]]) for phone in phones])
+                    
                 entry.phones=new_phones
             scrubbed_entries.append(entry)
         scrub_dct[r]=scrubbed_entries
     if settings.DEBUG:
         twins: dict = make_evil_twins(entries)
         for r in twins.keys():
-            entries = scrub_dct[r]
-            entries.insert(0, twins[r])
-            scrub_dct[r] = entries
+            _entries = scrub_dct[r]
+            _entries.insert(0, twins[r])
+            scrub_dct[r] = _entries
     return scrub_dct
 
 def normalize_role(role):
