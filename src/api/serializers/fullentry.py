727dcf3afa483d9942eb941eb421d426a6151f54
Jerome Pinguet <jerome@jerome.cc>
2026-01-08 11:52:30 +0100

    fix

--- src/api/serializers/fullentry.py
+++ src/api/serializers/fullentry.py
@@ -1,9 +1,12 @@
+import logging
 from directory.utils import find_entry, async_find_entry
 from api.utils import process
 from directory.tasty.fulleffectors import createEffectorRessource
 from api.types.fullentry import FullEntry
 from fastapi import Request, HTTPException, status
 from api.auth import normalize_role
+
+logger=logging.getLogger(__name__)
 
 def get_fullentry(uid):
     entry_node = find_entry(uid=uid)
@@ -16,6 +19,7 @@ async def async_get_fullentry(uid: str, req: Request, role: str, jwt)->FullEntry
     entry_node = await async_find_entry(uid=uid)
     entry_object = createEffectorRessource(entry_node)
     entry_dct = entry_object.__dict__
+    logger.debug(f"{entry_dct=}")
     attributes = ["phones, emails"]
     scrub_entry = process(entry_dct, role, attributes)
     entry_pydantic = FullEntry.model_validate(scrub_entry)
