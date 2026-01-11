298a68165da6a9f408e567e20130cbd5228c437e
Jerome Pinguet <jerome@jerome.cc>
2026-01-11 15:31:26 +0100

    somed types

--- src/api/routers/socialmedia.py
+++ src/api/routers/socialmedia.py
@@ -17,7 +17,7 @@ router = APIRouter()
 
 @router.get("/socialmedia/types")
 async def get_types():
-    return SocialNetwork.SocialNetworkType
+    return SocialNetwork.SocialNetworkType.__dict__
 
 @router.delete("/socialmedia/{item_id}")
 async def delete_item(item_id: str, request: Request, jwt: Annotated[dict, Depends(JWT)]):
