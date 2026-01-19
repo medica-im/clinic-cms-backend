1444c52a7306cd2e0c2c48e01ee6586257f8b84b
Jerome Pinguet <jerome@jerome.cc>
2026-01-19 16:36:10 +0100

    fix multiple entry post

--- src/api/serializers/entries.py
+++ src/api/serializers/entries.py
@@ -126,7 +126,8 @@ async def create_entry(entry: EntryPost, request: Request, jwt)-> FullEntry:
     for uid in entry_uids:
         _entry: Entry = await AsyncEntry.nodes.get(uid=uid)
         createdAt = _entry.createdAt
-        if createdAt and (ms - createdAt)<5000:
+        if createdAt and (ms - createdAt)<(1000*60*5):
+            await clear_cache("v2:entries", request)
             return await async_get_fullentry(str(_entry.uid), request, roles, jwt)
     new_entry = await entry_if_exists(effector, effector_type, facility)
     if not new_entry:
