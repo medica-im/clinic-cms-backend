6d73e9ae2a92691059e1b7e0b4a1e1efefe834b4
Jerome Pinguet <jerome@jerome.cc>
2026-02-20 14:45:33 +0100

    JWT

--- src/api/routers/fullentries.py
+++ src/api/routers/fullentries.py
@@ -15,7 +15,7 @@ async def get(uid: str, req: Request, jwt: Annotated[dict, Depends(check_cookie_
     return await async_get_fullentry(uid, req, jwt)
 
 @router.get("/slugfullentries/{type}/{commune}/{effector}")
-async def get_slug_fullentries(req: Request, type: str, commune: str, effector: str, jwt: Annotated[dict, Depends(JWT)]) -> FullEntry|None:
+async def get_slug_fullentries(req: Request, type: str, commune: str, effector: str, jwt: Annotated[dict, Depends(check_cookie_jwt)]) -> FullEntry|None:
     active=None
     uid = await slug_find_entry(commune, effector, type, active)
     return await async_get_fullentry(uid, req, jwt)
