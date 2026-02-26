de95abce614da78c6b3b185af3a967be17658b57
Jerome Pinguet <jerome@jerome.cc>
2026-02-26 14:42:29 +0100

    update message

--- src/api/serializers/invitee.py
+++ src/api/serializers/invitee.py
@@ -30,18 +30,19 @@ async def notification_email(invitee: Invitee, site: Site):
     subject = f"{organization.formatted_name} vous invite à utiliser le service {domain}"
     message = (
         f"Bonjour {invitee.name if invitee.name else ''}!\n\n"
-        f"{organization.formatted_name} vous invite à créer un compte sur le nouveau "
+        f"{organization.formatted_name} vous invite à créer un compte sur le "
         f"service en ligne {domain}. Vous pouvez vous rendre à l'adresse suivante:\n\n"
         f"https://{domain}/signin\n\n"
         f"et cliquer sur \"Se connecter avec Google\". Vous devez utiliser l'adresse "
         f"mail suivante: {invitee.email} Si vous n'avez pas de compte Google lié à "
         f"cette adresse, vous pourrez en créer un gratuitement en moins d'une minute. "
-        f"Il n'est pas nécessaire de créer une adresse Gmail, votre mail habituel "
-        f"{invitee.email} est suffisant.\n\n"
+        f"Il n'est pas nécessaire de créer une adresse Gmail! Votre mail habituel {invitee.email} est suffisant.\n\n"
+        f"""Si vous devez créer un compte Google, lors de l'étape "Méthode de connexion au compte", ne remplissez pas le champ "Nom d'utilisateur  ...@gmail.com". Cliquez sur "Utiliser l'adresse email existante".\n\n"""
         f"Après authentification par le service \"Se connecter avec Google\", votre "
         f"compte sur {domain} sera créé automatiquement. Vous pourrez utiliser nos "
         f"services et créer votre entrée dans l'annuaire de l'organisation.\n\n"
-        f"En cas de problème, merci de nous contacter via https://{domain}/contact"
+        f"En cas de problème, merci de nous contacter via https://{domain}/contact\n\n"
+        "Si vous souhaitez utiliser une autre adresse électronique pour vous connecter à notre service, contactez-nous et nous vous enverrons une nouvelle invitation."
     )
     logger.debug(f"{invitee.email=} {subject=} {message=}")
     send_single_email_task.delay(invitee.email, subject, message)
