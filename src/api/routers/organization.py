f112468329f12b034324cc16dad282db1f880a88
Jerome Pinguet <jerome@jerome.cc>
2026-07-07 17:20:29 +0200

    prevent absent neo4j Entry error for inactive Organization

--- src/api/routers/organization.py
+++ src/api/routers/organization.py
@@ -15,7 +15,7 @@ def organization(request: Request) -> OrganizationPy:
     logger.debug(f"{request.client=}")
     site = sync_get_site_from_request(request)
     try:
-        org = Organization.objects.select_related('contact').get(site=site)
+        org = Organization.objects.select_related('contact').get(site=site, active=True)
     except Organization.DoesNotExist:
         raise HTTPException(status_code=404, detail="Organization not found")
     serializer = OrganizationSerializer(org)
